f426fbda5afb824a81b26ca8973d3489d3c84e73
Joyal <joyal.palackel19@gmail.com>
2021-08-03 16:02:28 +0530

    day 2

--- factRec.java
+++ factRec.java
@@ -5,18 +5,18 @@ public class factRec {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
 
-        int res = factRec(n);
+        int res = disp(n);
         System.out.println(res);
     }
     
-    public static int factRec(int n) {
+    public static int disp(int n) {
 
         // base case: 0! = 1
         if(n==0){
             return 1;
         }
         // faith: gives factorial of n-1
-        int fnm1 = factRec(n-1);
+        int fnm1 = disp(n-1);
         // my work
         int res = n*fnm1;
         return res;
