cabc8c7f416d3a43bcf645b92c801bd5fe0a5595
FalconHex <joyal.palackel19@gmail.com>
2021-08-25 22:00:21 +0530

    Day 24

--- targetSumSubsetDP.java
+++ targetSumSubsetDP.java
@@ -29,31 +29,23 @@ public class targetSumSubsetDP {
     }
     
     public static boolean targetSumSubset(int[] arr, int target){
-        
         boolean[][] dp = new boolean[arr.length][target+1];
-        
         // dp[i][j] = if subset till i can give sum = j
-        
         for(int i=0; i<dp.length; i++){
             for(int j=0; j<dp[0].length; j++){
-                
                 if(j == 0){
                     // target 0 
                     dp[i][j] = true;
                 }
                 else if(i == 0){
-                    
                     if(arr[i] <= target){
                         // col == value of elem => single elem 0th row
                         dp[i][arr[i]] = true;
                     }
-                    
                 }
                 else{
                     boolean exc = dp[i-1][j];
-                    
                     boolean inc = false;
-                    
                     // if elem going to be included is 
                     // smaller than target - sum till now
                     if(j-arr[i] >= 0){
@@ -61,7 +53,6 @@ public class targetSumSubsetDP {
                         // diff of col and elem is achivable
                         inc = dp[i-1][j-arr[i]];
                     }
-                    
                     // if anyone is true place true
                     dp[i][j] = inc || exc;
                 }
